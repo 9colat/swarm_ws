18c74d00644362c56b959e8a4368d925c0e37fcd
9colat <nsaren17@student.aau.dk>
2021-09-13 10:59:36 +0200

    hey

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/nicoleg/swarm_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/nicoleg/swarm_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/nicoleg/swarm_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/nicoleg/swarm_ws/src:$ROS_PACKAGE_PATH"
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/nicoleg/swarm_ws/build/gtest/cmake_install.cmake")
+  include("/home/nicoleg/swarm_ws/build/hardware_driver/cmake_install.cmake")
 
 endif()
 
