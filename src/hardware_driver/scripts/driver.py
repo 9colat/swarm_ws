c5135eba76f9f55015c9c1ebacf05e4e6dbb4be0
9colat <nsaren17@student.aau.dk>
2021-09-13 12:22:45 +0200

    more update

--- src/hardware_driver/scripts/driver.py
+++ src/hardware_driver/scripts/driver.py
@@ -6,17 +6,14 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
+#          the node that the system is looking at
+#                        |
+#                        v
+    rospy.Subscriber("chatter", String, callback)
+    
     rospy.spin()
 
 if __name__ == '__main__':
