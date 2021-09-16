22f059be1594397b15f00bd49163c8e252cb4165
9colat <34935989+9colat@users.noreply.github.com>
2021-09-16 14:48:34 +0200

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/casper/swarm_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/casper/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/casper/catkin_ws/devel;/opt/ros/noetic' != '' else []
