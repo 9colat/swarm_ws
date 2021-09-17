9d41096cdece75d18c26071ab2461b53347e816f
9colat <nsaren17@student.aau.dk>
2021-09-17 14:19:16 +0200

    more more more done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # swarm_ws
-when you want to run the code with the teensy/arduino then you have to run a set of camands in the terminal.
+when you want to run the code with the teensy/arduino then you have to run a set of comands in the terminal.
 frist you have to start the ros master:
 
 roscore
@@ -9,3 +9,11 @@ then you have to set up the conection with the serial port:
 rosrun rosserial_python serial_node.py /dev/ttyACM0
 
 note that the port might differ between mechines so to find it you go to the arduino ide and go to the tool/port and there you will find the port your teensy/arduino is conected to.  
+
+to run the test controller code then then run the following comands
+
+rosrun hardware_driver driver.py
+
+>if this does not work then go to the swarm_ws/src/hardware_driver/scripts and run the following comande, the reason that this is needed is because the system does not concider the script executable:
+
+chmod 777 driver.py 
--- src/hardware_driver/scripts/driver.py
+++ src/hardware_driver/scripts/driver.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int16
+from geometry_msgs.msg import Vector3
 
 def talker():
-    pub = rospy.Publisher('pwm_sig', Int16, queue_size=10)
+    pub = rospy.Publisher('pwm_sig', Vector3, queue_size=10)
     pub1 = rospy.Publisher('dir_sig', Int16, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        temp_dir_mode = input("please enter the direction mode you want 0: strait mode, 1: turning mode")
-        temp_pwm = input("Enter pwm here:")
-        print(type(temp_pwm))
-        #if type(temp_var) == int:
-            #pwm_comand = temp_var
-        #if type(temp_var) == !int:
-            #pwm_comand = int(temp_var)
+        temp_dir_mode = input("please enter the direction mode you want 0: strait mode, 1: turning mode:")
+        temp_pwm_r = input("Enter right pwm here:")
+        temp_pwm_l = input("Enter left pwm here:")
+        temp_heading = input("Enter the heading angle here:")
+
         dir_comand = int(temp_dir_mode)
-        pwm_comand = int(temp_pwm)
-        rospy.loginfo(dir_comand)
-        pub1.publish(dir_comand)
+
+        pwm_comand = Vector3()
+        pwm_comand.x = int(temp_pwm_r)
+        pwm_comand.y = int(temp_pwm_l)
+        pwm_comand.z = int(temp_heading)
         rospy.loginfo(pwm_comand)
         pub.publish(pwm_comand)
+        rospy.loginfo(dir_comand)
+        pub1.publish(dir_comand)
         rate.sleep()
 
 if __name__ == '__main__':
