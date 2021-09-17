1b01abd45380d601d08f600d9d9b8a460b357912
9colat <nsaren17@student.aau.dk>
2021-09-17 10:21:47 +0200

    more done

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ frist you have to start the ros master:
 roscore
 then you have to set up the conection with the serial port:
 rosrun rosserial_python serial_node.py /dev/ttyACM0
-# note that the port might differ between mechines so to find it you go to the arduino ide and go to the tool/port and there you will find the port your teensy/arduino is conected to.  
+note that the port might differ between mechines so to find it you go to the arduino ide and go to the tool/port and there you will find the port your teensy/arduino is conected to.  
--- src/hardware_driver/scripts/driver.py
+++ src/hardware_driver/scripts/driver.py
@@ -1,21 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-def listener():
-    rospy.init_node('listener', anonymous=True)
-
-
-#          the node that the system is looking at
-
-#                        |
-#                        v
-    rospy.Subscriber("chatter", String, callback)
-
-    rospy.spin()
+def talker():
+    pub = rospy.Publisher('pwm_sig', Int16, queue_size=10)
+    pub1 = rospy.Publisher('dir_sig', Int16, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        temp_dir_mode = input("please enter the direction mode you want 0: strait mode, 1: turning mode")
+        temp_pwm = input("Enter pwm here:")
+        print(type(temp_pwm))
+        #if type(temp_var) == int:
+            #pwm_comand = temp_var
+        #if type(temp_var) == !int:
+            #pwm_comand = int(temp_var)
+        dir_comand = int(temp_dir_mode)
+        pwm_comand = int(temp_pwm)
+        rospy.loginfo(dir_comand)
+        pub1.publish(dir_comand)
+        rospy.loginfo(pwm_comand)
+        pub.publish(pwm_comand)
+        rate.sleep()
 
 if __name__ == '__main__':
-    listener()
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
