0c7b8d2358b65201ec58324f0e0df144d613cd77
Flooe <clfm17@student.aau.dk>
2021-09-22 12:42:56 +0200

    IMU_new.ino is working

--- arduino_code/IMU_ROS/IMU_ROS.ino
+++ arduino_code/IMU_ROS/IMU_ROS.ino
@@ -1,71 +1,24 @@
-#include <MPU9250_asukiaaa.h>
 #include <ros.h>
 #include <Adafruit_BMP280.h>
+#include <MPU9250_asukiaaa.h>
+#include <std_msgs/Int16.h>
 #include <geometry_msgs/Vector3.h>
 
-Adafruit_BMP280 bme; // I2C
-MPU9250_asukiaaa mySensor;
-float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
 
 ros::NodeHandle nh;
-geometry_msgs::Vector3 data;
-ros::Publisher IMU_pub("IMU_data", &data);
+//std_msgs::Int16 gyro_data;
+geometry_msgs::Vector3 gyro_data;
+ros::Publisher IMU_pub("mode_repeat", &gyro_data);  //here the publisher is initilazed with the publisher "name" the topic "name" and a pointer to the variable that is sent
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("start up");
+  Serial.println("Start up");
   nh.initNode();
-  while (!Serial);
-#ifdef _ESP32_HAL_I2C_H_ // For ESP32
-  Wire.begin(SDA_PIN, SCL_PIN);
-  mySensor.setWire(&Wire);
-#endif
-  bme.begin();
-  mySensor.beginAccel();
-  mySensor.beginGyro();
-  mySensor.beginMag();
-  // You can set your own offset for mag values
-  // mySensor.magXOffset = -50;
-  // mySensor.magYOffset = -55;
-  // mySensor.magZOffset = -10;
   nh.advertise(IMU_pub);
 }
+
 void loop() {
-
-  if (mySensor.accelUpdate() == 0) {
-    aX = mySensor.accelX();
-    aY = mySensor.accelY();
-    aZ = mySensor.accelZ();
-    aSqrt = mySensor.accelSqrt();
-    Serial.print("accelX: " + String(aX));
-    Serial.print("\taccelY: " + String(aY));
-    Serial.print("\taccelZ: " + String(aZ));
-    Serial.print("\taccelSqrt: " + String(aSqrt));
-  }
-
-  if (mySensor.gyroUpdate() == 0) {
-    gX = mySensor.gyroX();
-    gY = mySensor.gyroY();
-    gZ = mySensor.gyroZ();
-    Serial.print("\tgyroX: " + String(gX));
-    Serial.print("\tgyroY: " + String(gY));
-    Serial.print("\tgyroZ: " + String(gZ));
-  }
-
-  if (mySensor.magUpdate() == 0) {
-    mX = mySensor.magX();
-    mY = mySensor.magY();
-    mZ = mySensor.magZ();
-    mDirection = mySensor.magHorizDirection();
-    Serial.print("\tmagX: " + String(mX));
-    Serial.print("\tmaxY: " + String(mY));
-    Serial.print("\tmagZ: " + String(mZ));
-    Serial.print("\thorizontalDirection: " + String(mDirection));
-  }
-
-  Serial.println(""); // Add an empty line
-  data.x=gX;
-  data.y=gY;
-  data.z=gZ;
-  IMU_pub.publish(&data);
-  }
+  gyro_data.x = 1;
+  IMU_pub.publish(&gyro_data);
+  nh.spinOnce();
+}
--- arduino_code/IMU_new.ino
+++ arduino_code/IMU_new.ino
@@ -1,37 +1,24 @@
 #include <ros.h>
+#include <Adafruit_BMP280.h>
+#include <MPU9250_asukiaaa.h>
 #include <std_msgs/Int16.h>
 #include <geometry_msgs/Vector3.h>
 
 
-ros::NodeHandle nh;                 // here the node handler is set with the name nh
-std_msgs::Int16 mode_confurm;       // the variable is initilazed as a Int16, this is a ros type that is the type that you can sent over the ros topics
-ros::Publisher mode_pub("mode_repeat", &mode_confurm);  //here the publisher is initilazed with the publisher "name" the topic "name" and a pointer to the variable that is sent
-
-void array_push(long the_input_array[], float data){
-  for (int x = sizeof(the_input_array); x > 0; x = x - 1){
-    the_input_array[x] = the_input_array[x-1];
-  }
-  the_input_array[0] = data*float_to_long_factor;
-  }
-
-float array_sum(long the_input_array[]){
-  float result = 0;
-  for (int x = 0; x < sizeof(the_input_array); x++){
-    result = result + (the_input_array[x]/float_to_long_factor);
-  }
-  return result;
-  }
-
+ros::NodeHandle nh;
+//std_msgs::Int16 gyro_data;
+geometry_msgs::Vector3 gyro_data;
+ros::Publisher IMU_pub("mode_repeat", &gyro_data);  //here the publisher is initilazed with the publisher "name" the topic "name" and a pointer to the variable that is sent
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Start up");
   nh.initNode();
-  nh.advertise(mode_pub);
+  nh.advertise(IMU_pub);
 }
 
 void loop() {
-  mode_confurm.data = mode_mode;
-  mode_pub.publish(&mode_confurm);
+  gyro_data.x = 1;
+  IMU_pub.publish(&gyro_data);
   nh.spinOnce();
 }
