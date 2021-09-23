25492430af478346b65401e6d389e1d3f27bb675
Flooe <clfm17@student.aau.dk>
2021-09-23 10:31:04 +0200

    IMU CODE WORKING

--- arduino_code/IMU_ROS/IMU_wire.ino
+++ arduino_code/IMU_ROS/IMU_wire.ino
@@ -2,8 +2,8 @@
 #include <std_msgs/String.h>
 #include <Wire.h>
 
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+const int MPU_addr=0x68;  // I2C address of the MPU-6050 and mpu9250
+int16_t Accel_X,Accel_Y,Accel_Z,Tmp,Gyro_X,Gyro_Y,Gyro_Z;
 
 
 //Set up the ros node and publisher
@@ -12,13 +12,13 @@ ros::Publisher imu("imu", &imu_msg);
 ros::NodeHandle nh;
 
 
-                           
+
 void setup()
 {
 
   nh.initNode();
   nh.advertise(imu);
-  
+
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
@@ -35,20 +35,20 @@ void loop()
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers  String AX = String(mpu6050.getAccX());
-  
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+  Accel_X=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  Accel_Y=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  Accel_Z=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
   Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  String AX = String(AcX);
-  String AY = String(AcY);
-  String AZ = String(AcZ);
-  String GX = String(GyX);
-  String GY = String(GyY);
-  String GZ = String(GyZ);
+  Gyro_X=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  Gyro_Y=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  Gyro_Z=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  String AX = String(Accel_X);
+  String AY = String(Accel_Y);b
+  String AZ = String(Accel_Z);
+  String GX = String(Gyro_X);
+  String GY = String(Gyro_Y);
+  String GZ = String(Gyro_Z);
   String tmp = String(Tmp);
 
   String data = "A" + AX + "B"+ AY + "C" + AZ + "D" + GX + "E" + GY + "F" + GZ + "G" ;
@@ -56,7 +56,7 @@ void loop()
   int length = data.indexOf("G") +2;
   char data_final[length+1];
   data.toCharArray(data_final, length+1);
-  
+
   if (millis() > publisher_timer) {
     // step 1: request reading from sensor
     imu_msg.data = data_final;
@@ -65,5 +65,5 @@ void loop()
     nh.spinOnce();
   }
 
-  
+
 }
