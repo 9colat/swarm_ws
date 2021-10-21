3d20c9ed1151b2f0ac59c7b28dbe5e974d0d8751
9colat <nsaren17@student.aau.dk>
2021-10-21 18:48:28 +0200

    fixed the rosserial

--- my_setup.sh
+++ my_setup.sh
@@ -12,7 +12,7 @@ else
     sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security universe"
     sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security multiverse"
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt install curl -y# if you haven't already installed curl
+    sudo apt install curl # if you haven't already installed curl
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update -y
     sudo apt upgrade -y
@@ -27,8 +27,8 @@ else
 fi
 
 #setting up the arduino ide and libraries
-sudo apt install ros-noetic-rosserial-arduino
-sudo apt install ros-noetic-rosserial
+sudo apt install ros-noetic-rosserial-arduino -y
+sudo apt install ros-noetic-rosserial -y
 
 
 echo "Download and installing arduino ide"
