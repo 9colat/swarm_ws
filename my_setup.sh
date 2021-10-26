bee8ec2dc41d90b7544d655e3031fac7d3fdc5b5
9colat <nsaren17@student.aau.dk>
2021-10-26 11:47:15 +0200

    minor fix

--- my_setup.sh
+++ my_setup.sh
@@ -12,7 +12,7 @@ else
     sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security universe"
     sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security multiverse"
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt install curl # if you haven't already installed curl
+    sudo apt install curl
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update -y
     sudo apt upgrade -y
@@ -34,7 +34,7 @@ sudo apt upgrade -y
 
 echo "Download and installing arduino ide"
 cd Downloads/
-curl -o arduino-1.8.15-linuxaarch64.tar.xz https://downloads.arduino.cc/arduino-1.8.16-linuxaarch64.tar.xz
+curl -o arduino-1.8.15-linuxaarch64.tar.xz https://downloads.arduino.cc/arduino-1.8.15-linuxaarch64.tar.xz
 tar -xf arduino-1.8.15-linuxaarch64.tar.xz -C /home/$USER/
 cd
 cd arduino-1.8.15
@@ -73,13 +73,13 @@ cd ..
 catkin_make
 source devel/setup.bash
 
-sudo cp ~/swarm_ws/disk-space-check.sh /usr/local/bin/disk-space-check.sh
-sudo chmod 744 /usr/local/bin/disk-space-check.sh
-sudo chmod 664 /etc/systemd/system/disk-space-check.service
+#sudo cp ~/swarm_ws/disk-space-check.sh /usr/local/bin/disk-space-check.sh
+#sudo chmod 744 /usr/local/bin/disk-space-check.sh
+#sudo chmod 664 /etc/systemd/system/disk-space-check.service
 
 cd
-sudo apt install arduino-mk -y
-cd /home/nicoleg/arduino-1.8.15/hardware/teensy/avr/cores/teensy3
+#sudo apt install arduino-mk -y
+#cd /home/nicoleg/arduino-1.8.15/hardware/teensy/avr/cores/teensy3
 
 #installing brew package mangerger
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -88,12 +88,6 @@ echo "eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"" >> ~/.bashrc
 
 brew install arduino-cli -y
 sudo apt install teensy-loader-cli -y
-
-
-
-
-#cd swarm_ws/arduino_code/motor_control
-#make upload
-
-
-#ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCk0mw8PVa00e9XQmEksm77+NieO8pwDGXSheLAyK8PkcDDsTv3OodFuxx0ca0Tx79rq7j05Q7lEKmXtZ2UiwkP7H3q8v/YXoOQgQyiVqJjUrTfN4JwLQbPoVkZTMuyGP6+iTlCr9VCNv0W9nE3t2/1GXUKZNUKGP/vEqggAFCBQ0+GARh0OosWP3NBeJztSzJqtQpb6B0g7OFbqEQLiJJ/PMxn5vUVUpmsi0vVbNch6VzC4n6dURH5cXvIzU2lm0cljObncxH7EYRtXfHWxRyICMOmdD7hO6OWzqvrEbGOEBLsJjvD7ZNtLvWSqQaOaPrsb7rvQM5BeEAJ6h7/VxcBzaaI68ljLULG6IHRNQlW2+4t4Amg6DxYy1zlsnw/ZcsGBBt4hUsNthnvnkn/AmCLIHSoxQyn6y7sCzCS75NBChyaygnq7HTNiCLngL2qbyvp9PpUglaIBLW4iK1OEN8R2BgpCjKfGKNol2LaQp4sE43YIPI81SFyz2D7iiGdxLc= nicoleg@nicoleg-ThinkPad-X250
+cd
+chmod 777 /swarm_ws/arduino_make_upload.sh
+chmod 777 /swarm_ws/start_up_script.sh
