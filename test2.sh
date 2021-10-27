e4214172f1a7e6fd772909aed1acc80358de6ab7
9colat <nsaren17@student.aau.dk>
2021-10-27 14:43:47 +0200

    fixed Makefile fixed arduino-cli.yaml

--- test2.sh
+++ test2.sh
@@ -4,6 +4,7 @@ echo "hey"
 arduino-cli config init
 cd .arduino15
 perl -pi -e 's/always_export_binaries: false/always_export_binaries: true/g' arduino-cli.yaml
+perl -pi -e 's/user: /home/nicoleg/Arduino/user: /home/nicoleg/arduino-1.8.15/g' arduino-cli.yaml
 echo "whats wrong with your face?"
 cd
 arduino-cli core update-index
