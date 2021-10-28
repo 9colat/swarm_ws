aa83fc4f7a11f53c0e5f6d93582fca8a5d3d2eb3
9colat <nsaren17@student.aau.dk>
2021-10-28 11:29:56 +0200

    added timestamping

--- test1.sh
+++ test1.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo "hey"
-echo "export PATH=\$PATH:/home/pi/bin" >> ~/.bashrc
-gnome-terminal -x sh -c "./test2.sh; bash"
+echo "$(date)" >> ~/time_stamp.txt
+sleep 60
+gnome-terminal -x sh -c "./test1.sh; bash"
 echo "whats wrong with your face?"
+kill $PPID
