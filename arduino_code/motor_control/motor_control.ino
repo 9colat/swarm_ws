3ca32e21d7f9b737a75ad3602d18f3639b5c3e33
9colat <nsaren17@student.aau.dk>
2021-11-17 15:19:33 +0100

    dav

--- arduino_code/motor_control/motor_control.ino
+++ arduino_code/motor_control/motor_control.ino
@@ -77,6 +77,31 @@ std_msgs::Float64 wheel_speed;
 ros::Publisher speed_pub("wheel_speed", &wheel_speed);
 
 
+struct Quaternion
+{
+    double w, x, y, z;
+};
+
+Quaternion ToQuaternion(double yaw, double pitch, double roll) // yaw (Z), pitch (Y), roll (X)
+{
+    // Abbreviations for the various angular functions
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+
+    Quaternion q;
+    q.w = cr * cp * cy + sr * sp * sy;
+    q.x = sr * cp * cy - cr * sp * sy;
+    q.y = cr * sp * cy + sr * cp * sy;
+    q.z = cr * cp * sy - sr * sp * cy;
+
+    return q;
+}
+
+
 void array_push(long the_input_array[], float data) {
   for (int x = sizeof(the_input_array); x > 0; x = x - 1) {
     the_input_array[x] = the_input_array[x - 1];
--- src/local_avoider/scripts/local_avoider.py
+++ src/local_avoider/scripts/local_avoider.py
@@ -1,1 +1,66 @@
-#!/usr/bin/env python
+#! /usr/bin/env python3
+import rospy
+
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Path
+from std_msgs.msg import Int16
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import MultiArrayDimension
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import LaserScan
+import math
+import numpy
+lidar_array = [0] * 360
+
+def callback_lidar(data):
+    for i in range(len(data.ranges)):
+        lidar_array[i] = data.ranges[i]
+        if lidar_array[i] < 0.3:
+            pub_signal = rospy.Publisher('danger_signal', Vector3, queue_size=10)
+            pub_quat = rospy.Publisher('danger_quar', Quaternion, queue_size=10)
+            j = 0
+            q = quaternion_from_euler(0, 0, math.radians(i))
+            quat_msg = Quaternion(q[0],q[1],q[2],q[3])
+            mat = Vector3()
+            mat.x = data.ranges[i]
+            mat.y = i
+            mat.z = j
+            print("You are too close at angle: "+str(i))
+            pub_signal.publish(mat)
+            pub_quat.publish(quat_msg)
+            print(q)
+            j = j + 1
+        if lidar_array[i] > 0.3 and lidar_array[i] < 0.5:
+            print("hey i meet just you")
+    print(lidar_array[i])
+
+def cartian_to_polar(current_pose_x, current_pose_y, quat_x, quat_y, quat_z, quat_w , goal_pose_x, goal_pose_y, lidar_array_temp):
+    delta_x = current_pose_x - goal_pose_x
+    delta_y = current_pose_y - goal_pose_y
+    t3 = +2.0 * (quat_w * quat_z + quat_x * quat_y)
+    t4 = +1.0 - 2.0 * (quat_y * quat_y + quat_z * quat_z)
+    current_yaw = math.degrees(math.atan2(t3, t4))
+    current_theta = math.atan2(t3, t4)
+    theta_goal = numpy.arctan((delta_y/delta_x))
+    delta_theta = current_goal - theta_goal
+    if delta_theta > math.radians(0) and delta_theta < math.radians(180):
+        
+
+
+    return
+
+
+def lidar_data():
+    rospy.init_node('local_avoider', anonymous=True)
+    rospy.Subscriber("scan", LaserScan, callback_lidar)
+    print(cartian_to_polar(0,0,0,0,0.7072,0.7072,1,1,lidar_array))
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    lidar_data()
