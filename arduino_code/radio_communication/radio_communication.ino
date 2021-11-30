4c457ebbfef79a71a8c74871d328f275bdfbc9c5
romcioo <aleksandra.dudlikk@gmail.com>
2021-11-30 13:16:01 +0100

    code better organised

--- arduino_code/radio_communication/radio_communication.ino
+++ arduino_code/radio_communication/radio_communication.ino
@@ -14,20 +14,20 @@ String message_to_send;
 std_msgs::String data_to_be_received;
 
 
-
+// first we need to subscribe to data, so we can send it over the radio later
 void data_to_be_sent (const std_msgs::String& data_msg) {
   message_to_send = data_msg.data;
   Serial.println(message_to_send);
-  radio_module.write(&message_to_send); //sending data
+  radio_module.write(&message_to_send); //now the subscribed data is being sent over the radio
 }
 
+ros::Subscriber<std_msgs::String> subscriber("data_to_be_sent", data_to_be_sent ); //subscriber setup
+ros::Publisher publisher("data_to_be_received", &data_to_be_received); //publisher setup
 
 
-ros::Subscriber<std_msgs::String> subscriber("data_to_be_sent", data_to_be_sent );
-ros::Publisher publisher("data_to_be_received", &data_to_be_received);
 
 
-
+// first we need to receive the data over the radio, so we can publish it later
 void receiving() {
   if (radio_module.available() > 0) { // is there something on the radio? (enable the radio)
 
@@ -47,7 +47,7 @@ void receiving() {
     }
 
     data_to_be_received.data = str;
-    publisher.publish(&data_to_be_received);
+    publisher.publish(&data_to_be_received); //now the read data is being published through ROS
   }
 }
 
@@ -55,13 +55,13 @@ void receiving() {
 
 void setup() {
 
-  Serial.begin(115200); // terminal
+  Serial.begin(57600); // terminal
   radio_module.begin(9600); // radio module
 
   nh.initNode();
   nh.advertise(publisher);
   nh.subscribe(subscriber);
-  
+
 }
 
 
@@ -69,8 +69,7 @@ void setup() {
 void loop() {
 
   Serial.println("Start the main loop");
-  data_to_be_received.data = "hello";
   receiving();
   nh.spinOnce();
-  
+
 }
