34eb62a12377474ed2de88de8305a4e5cd1a1fdf
romcioo <aleksandra.dudlikk@gmail.com>
2021-11-30 12:50:13 +0100

    ros communication added

--- arduino_code/radio_communication/radio_communication.ino
+++ arduino_code/radio_communication/radio_communication.ino
@@ -1,9 +1,55 @@
 #include <SoftwareSerial.h>
+#include <ros.h>
+#include <std_msgs/String.h>
 
+ros::NodeHandle  nh;
 SoftwareSerial radio_module(4, 7); // TX 4, RX 7
+
+
+
 
 const int message_size = 63; // how many chars the message can contain
 char str[message_size]; // array of chars initialised, that will contain the sent message
+String message_to_send;
+std_msgs::String data_to_be_received;
+
+
+
+void data_to_be_sent (const std_msgs::String& data_msg) {
+  message_to_send = data_msg.data;
+  Serial.println(message_to_send);
+  radio_module.write(&message_to_send); //sending data
+}
+
+
+
+ros::Subscriber<std_msgs::String> subscriber("data_to_be_sent", data_to_be_sent );
+ros::Publisher publisher("data_to_be_received", &data_to_be_received);
+
+
+
+void receiving() {
+  if (radio_module.available() > 0) { // is there something on the radio? (enable the radio)
+
+    Serial.println("Radio module available");
+
+    for (int i = 0; i < message_size; i++) {
+
+      Serial.print("Reading  ");
+      char radio_data = char(radio_module.read()); // reading data in chars, saved in radio_data char
+      str[i] = radio_data; // radio_data char passed to the str char array
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(str[i]);
+
+      //FILTER STUFF THAT IS DUMB AND YOU DONT WANT TO PASS IT INTO THE ARRAY
+      Serial.println(str);
+    }
+
+    data_to_be_received.data = str;
+    publisher.publish(&data_to_be_received);
+  }
+}
 
 
 
@@ -12,6 +58,10 @@ void setup() {
   Serial.begin(115200); // terminal
   radio_module.begin(9600); // radio module
 
+  nh.initNode();
+  nh.advertise(publisher);
+  nh.subscribe(subscriber);
+  
 }
 
 
@@ -19,41 +69,8 @@ void setup() {
 void loop() {
 
   Serial.println("Start the main loop");
-
-  if (radio_module.available() > 0) { // is there something on the radio? (enable the radio)
-
-    Serial.println("Radio module available");
-
-    for (int i = 0; i < message_size; i++) {
-
-      Serial.print("Reading  ");
-      //Serial.println(char(radio_module.read())); //reading data
-      char radio_data = char(radio_module.read()); // reading data in chars, saved in radio_data char
-      str[i] = radio_data; // radio_data char passed to the str char array
-      Serial.print(i);
-      Serial.print(" ");
-      Serial.println(str[i]);
-      //delay(1000);
-    }
-
-    Serial.println(str);
-    String radio_data_in_string_format = str; // radio_data that is in the str char array is now converted to a string XD
-
-    Serial.println(radio_data_in_string_format);
-
-  }
-
-
-
-
-  delay(1000);
-
-
-
-
-
-  //radio_module.write(35); //sending data
-  //delay(10000);
-
-
+  data_to_be_received.data = "hello";
+  receiving();
+  nh.spinOnce();
+  
 }
