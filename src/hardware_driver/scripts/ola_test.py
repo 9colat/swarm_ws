33dae2e0496e5582ec0521c8ff6d8c6a0f538752
9colat <nsaren17@student.aau.dk>
2021-11-30 11:56:58 +0100

    hehe

--- src/hardware_driver/scripts/ola_test.py
+++ src/hardware_driver/scripts/ola_test.py
@@ -5,7 +5,6 @@ from std_msgs.msg import String
 
 
 def talker():
-    #pub = rospy.Publisher('pwm_sig', Vector3, queue_size=10)
     pub1 = rospy.Publisher('data_to_be_sent', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
@@ -13,10 +12,6 @@ def talker():
         dir_comand = input("please enter the direction mode you want 0: strait mode, 1: turning mode:")
         if dir_comand == "kill" or dir_comand == "quit" or dir_comand == "end" or dir_comand == "stop" or dir_comand == "q":
             sys.exit("kill compleat")
-        #temp_pwm_r = input("Enter right pwm here:")
-        #temp_pwm_l = input("Enter left pwm here:")
-        #temp_heading = input("Enter the heading angle here:")
-
 
         rospy.loginfo(dir_comand)
         pub1.publish(dir_comand)
