ace8c567192f0cb78b99e515081bc28c2e1cc811
9colat <nsaren17@student.aau.dk>
2021-12-15 13:39:42 +0100

    i made a launch file to test the start

--- start_up_script.sh
+++ start_up_script.sh
@@ -8,5 +8,13 @@ sudo apt upgrade -y
 cd
 cd swarm_ws/
 git pull origin master
+cd
+
+source ~/swarm_ws/devel/setup.bash
+roslaunch hardware_driver test.launch
+
+
+
+
 date > ~/check.txt
 du -sh /home/ >> ~/check.txt
