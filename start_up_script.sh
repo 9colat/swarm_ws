34fe30e7e5035ba23ed74fae7541ad15d79619af
9colat <nsaren17@student.aau.dk>
2022-01-13 15:11:46 +0100

    hehe

--- start_up_script.sh
+++ start_up_script.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+echo "Im starting"
 sleep 30
 
 #echo "å"|sudo -S /path/to/command
+echo "Here we go"
 sudo apt update -y
 sudo apt upgrade -y
-
+echo "doing the git stuff"
 cd
 cd swarm_ws/
 git pull origin master
@@ -15,6 +17,7 @@ source ~/swarm_ws/devel/setup.bash
 #cd swarm_ws
 #gnome-terminal -x sh -c "./test1.sh; bash"
 #roslaunch hardware_driver test.launch
+echo "$data" >> time_stamp.txt
 
 
 
