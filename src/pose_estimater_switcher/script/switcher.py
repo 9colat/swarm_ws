feae68fbab0534c43d1c52521ae018ebab63421e
romcioo <aleksandra.dudlikk@gmail.com>
2022-01-18 12:56:33 +0100

    subscribers for ups_pose and lidar_pose added

--- src/pose_estimater_switcher/script/switcher.py
+++ src/pose_estimater_switcher/script/switcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Pose
+from sensor_msgs.msg import LaserScan
 estimator_switch = 6 ## this number is in meters.
 pose_x = 0  ## just an initial coordinate in x
 pose_y = 0  ## just an initial coordinate in y
@@ -14,8 +15,23 @@ lidar_pose_z = 0 ## just an initial pose estimate from lidar in z
 
 ## here we need to add the Subscriber for the lidar position estimate and the ups.
 
+def callback_ups_pose(data):
+    global ups_pose_x, ups_pose_y, ups_poze_z
+    ups_pose_x = data.position.x
+    ups_pose_y = data.position.y
+    ups_pose_z = data.position.z
+    print(ups_pose_x)
+
+
+def callback_lidar_pose(data):
+    global ups_lidar_x, ups_lidar_y, ups_lidar_z
+    ups_lidar_x = data.position.x
+    ups_lidar_y = data.position.y
+    ups_lidar_z = data.position.z
+
+
 def callback_lidar(data):
-    global estimator_switch pose_x pose_y pose_z lidar_pose_x lidar_pose_y lidar_pose_z ups_pose_x ups_pose_y ups_pose_z
+    global estimator_switch, pose_x, pose_y, pose_z, lidar_pose_x, lidar_pose_y, lidar_pose_z, ups_pose_x, ups_pose_y, ups_pose_z
     for i in range(len(data.ranges)):
         lidar_array[i] = data.ranges[i]
     if min(lidar_array) < estimator_switch:
@@ -29,11 +45,13 @@ def callback_lidar(data):
 
 
 def switcher():
-    global pose_x pose_y pose_z
+    global pose_x, pose_y, pose_z
     rospy.init_node('switcher', anonymous=True)
     pub = rospy.Publisher('position', Pose, queue_size=10)
     while True:
         rospy.Subscriber("scan", LaserScan, callback_lidar)
+        rospy.Subscriber("ups_pose", Pose, callback_ups_pose)
+        rospy.Subscriber("lidar_pose", Pose, callback_lidar_pose)
 
         data = Pose()
         data.position.x = pose_x
