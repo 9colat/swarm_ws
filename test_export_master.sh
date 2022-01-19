c7e69a1c223402f8e34db639f8b0017db14656f7
9colat <nsaren17@student.aau.dk>
2022-01-19 14:21:18 +0100

    maybe we need rosrun instead of roslaunch

--- test_export_master.sh
+++ test_export_master.sh
@@ -3,7 +3,7 @@ echo "hey"
 gnome-terminal -x sh -c "./launch.sh; bash"
 sleep 1
 export ROS_MASTER_URI=http://hal:11311
-roslaunch hardware_driver test_1.launch
+rosrun hardware_driver driver.py
 #sleep 60
 #gnome-terminal -x sh -c "./test1.sh; bash"
 #echo "whats wrong with your face?"
