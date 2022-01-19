e3e111044c94191b3021e12a2dcfeda413ded481
9colat <nsaren17@student.aau.dk>
2022-01-19 14:01:50 +0100

    oops - one more

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+echo "${tm} roscore started" >> time_stamp.txt
 roscore
+echo "${tm} roscore stopped" >> time_stamp.txt
 #sleep 60
 #gnome-terminal -x sh -c "./test1.sh; bash"
 #echo "whats wrong with your face?"
