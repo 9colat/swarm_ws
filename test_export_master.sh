4dd45be01732494983e1ecdce3dabab8333e9277
9colat <nsaren17@student.aau.dk>
2022-01-19 14:46:21 +0100

    how about now, will you work wth this

--- test_export_master.sh
+++ test_export_master.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo "hey"
-gnome-terminal -x sh -c "./launch.sh; bash"
+#gnome-terminal -x sh -c "./launch.sh; bash"
 sleep 1
-export ROS_MASTER_URI=http://hal:11311
+export ROS_MASTER_URI=http://ubuntu:11311
 rosrun hardware_driver driver.py
 #sleep 60
 #gnome-terminal -x sh -c "./test1.sh; bash"
