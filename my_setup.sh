85243f990431251d75d274358d0438a8a23c76fc
Flooe <clfm17@student.aau.dk>
2022-01-24 10:52:42 +0100

    added update upgrade before rosinstall

--- my_setup.sh
+++ my_setup.sh
@@ -26,12 +26,16 @@ nmcli r wifi on
 sudo nmcli dev wifi connect testwifi
 
 echo "here ros is installing"
-#sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security main restricted"
+#sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu fstart_up_scriptocal-security main restricted"
 #sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security universe"
 #sudo add-apt-repository "deb-src http://security.ubuntu.com/ubuntu focal-security multiverse"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update -y
+sudo apt upgrade -y
+
 sudo apt install ros-noetic-desktop -y
 source /opt/ros/noetic/setup.bash
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
