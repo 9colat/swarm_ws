53292ceb375969345c3f2f21c44fa7d3c92e8319
9colat <nsaren17@student.aau.dk>
2022-01-25 10:07:13 +0100

    i misssed a thing

--- host_ros_launch.sh
+++ host_ros_launch.sh
@@ -5,7 +5,7 @@ IP=$(hostname -I)
 read A B C D <<<"${IP//./ }"
 export ROS_IP=$A.$B.$C.$D
 export ROS_MASTER_URI=http://$A.$B.$C.$D:11311
-roslaunch hardware_driver driver.launch
+roslaunch hardware_driver test_for_driving.launch
 #sleep 60
 #gnome-terminal -x sh -c "./test1.sh; bash"
 #echo "whats wrong with your face?"
