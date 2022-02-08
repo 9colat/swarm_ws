783cd90e857d9eac050798ceeab07cf702237f1e
Flooe <clfm17@student.aau.dk>
2022-02-08 10:38:16 +0100

    remove shite

--- clint_ros_launch.sh
+++ clint_ros_launch.sh
@@ -5,7 +5,7 @@ echo "hey"
 read A B C D <<<"${IP//./ }"
 export ROS_IP=$A.$B.$C.$D
 export ROS_MASTER_URI=http://$A.$B.$C.$D:11311
-rosrun hardware_driver logger_node.py
+#rosrun hardware_driver logger_node.py
 #sleep 60
 #gnome-terminal -x sh -c "./test1.sh; bash"
 #echo "whats wrong with your face?"
