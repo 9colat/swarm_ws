68351267dfa858a7a8b2d6b369680bd1f8c30432
9colat <nsaren17@student.aau.dk>
2022-02-16 17:21:00 +0100

    he

--- bash/arduino_maker.sh
+++ bash/arduino_maker.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
+cd
+
 arduino-cli compile --fqbn teensy:avr:teensy36 ~/swarm_ws/arduino_code/${1}/${1}.ino
+addad="no"
+if grep -q "!arduino_code/${1}/build" ~/swarm_ws/.gitignore
+  then
+    addad="yes"
+fi
+if [$addad = "no"]
+  then
+      echo "\!arduino_code/${1}/build" >> ~/swarm_ws/.gitignore
+fi
+
+cd
+cd swarm_ws
+git commit -m "auto upload from arduino maker"
+git push origin master
