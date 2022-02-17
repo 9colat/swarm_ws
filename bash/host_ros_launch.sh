85316e1a0c0e65a77b83adef7aa525998e07bb22
9colat <nsaren17@student.aau.dk>
2022-02-17 13:22:05 +0100

    peop

--- bash/host_ros_launch.sh
+++ bash/host_ros_launch.sh
@@ -2,12 +2,14 @@
 
 echo "hey"
 
+in=$1
+
 IP=$(hostname -I)
 read A B C D <<<"${IP//./ }"
 export ROS_IP=$A.$B.$C.$D
 export ROS_MASTER_URI=http://$A.$B.$C.$D:11311
 cd
-./swarm_ws/bash/code_runner $1
+./swarm_ws/bash/code_runner $in
 
 #roslaunch swarm_robot_nav on_robot.launch
 #sleep 60
