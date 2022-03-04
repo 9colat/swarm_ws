cbedc50eb3f9bc28328972e2417cf1e45b729098
9colat <nsaren17@student.aau.dk>
2022-03-04 17:12:44 +0100

    i try a thing ;)

--- arduino_code/motor_strait_driving/motor_strait_driving.ino
+++ arduino_code/motor_strait_driving/motor_strait_driving.ino
@@ -13,11 +13,11 @@
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Twist.h>
-#include <custom_msgs/odom_and_imu.h>
+//#include <custom_msgs/odom_and_imu.h>
 
-Adafruit_BMP280 bmp; // use I2C interface
-Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
-Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+//Adafruit_BMP280 bmp; // use I2C interface
+//Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+//Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 double error_l;
 double error_r;
 
@@ -127,8 +127,8 @@ geometry_msgs::Vector3 imu_mag = geometry_msgs::Vector3();
 ros::Publisher IMU_data_mag("imu_mag", &imu_mag);
 geometry_msgs::Vector3 data_measured_angle = geometry_msgs::Vector3();
 ros::Publisher measured_angle_pub("measured_angle", &data_measured_angle);
-custom_msgs::odom_and_imu data_measured_odom_and_imu = custom_msgs::odom_and_imu();
-ros::Publisher odom_and_IMU_pub("odometry_and_IMU", &data_measured_odom_and_imu);
+//custom_msgs::odom_and_imu data_measured_odom_and_imu = custom_msgs::odom_and_imu();
+//ros::Publisher odom_and_IMU_pub("odometry_and_IMU", &data_measured_odom_and_imu);
 
 
 //-----Functions-----//
@@ -361,15 +361,15 @@ void cmd_velocity(geometry_msgs::Twist& cmd_goal) {
 
 void imu_collection() {
   accelgyro.getMotion9(&accel_X, &accel_Y, &accel_Z, &gyro_X, &gyro_Y, &gyro_Z, &mx, &my, &mz);
-  data_measured_odom_and_imu.imu_acc.x = accel_X;
-  data_measured_odom_and_imu.imu_acc.y = accel_Y;
-  data_measured_odom_and_imu.imu_acc.z = accel_Z;
+  //data_measured_odom_and_imu.imu_acc.x = accel_X;
+  //data_measured_odom_and_imu.imu_acc.y = accel_Y;
+  //data_measured_odom_and_imu.imu_acc.z = accel_Z;
   //IMU_data_acc.publish(&imu_acc);
-  sensors_event_t temp_event;
-  bmp_temp->getEvent(&temp_event);
-  temperature = temp_event.temperature;
+  //sensors_event_t temp_event;
+  //bmp_temp->getEvent(&temp_event);
+  //temperature = temp_event.temperature;
 
-  data_measured_odom_and_imu.temp = temperature;
+  //data_measured_odom_and_imu.temp = temperature;
 
   imu_gyro.x = gyro_X;
   imu_gyro.y = gyro_Y;
@@ -482,20 +482,20 @@ void setup() {
   nh.advertise(IMU_data_acc);
   nh.advertise(IMU_data_gyro);
   nh.advertise(IMU_data_mag);
-  nh.advertise(odom_and_IMU_pub);
-  unsigned status;
-  status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-  if (!status) {
-    nh.loginfo("Could not find a valid BMP280 sensor, check wiring");
-  }
+  //nh.advertise(odom_and_IMU_pub);
+  //unsigned status;
+  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  //if (!status) {
+  //  nh.loginfo("Could not find a valid BMP280 sensor, check wiring");
+  //}
 
-  accelgyro.initialize();
-  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-  bmp_temp->printSensorDetails();
+  //accelgyro.initialize();
+  //bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+  //                Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+  //                Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+  //                Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+  //                Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  //bmp_temp->printSensorDetails();
 }
 
 
@@ -529,10 +529,10 @@ void loop() {
     wheel_speed.y = error_l;
     wheel_speed.z = cum_error_r;
     wheel_speed.w = cum_error_l;*/
-    imu_collection();
-    data_measured_odom_and_imu.omega_right = average_omega_right;
-    data_measured_odom_and_imu.omega_left = average_omega_left;
-    odom_and_IMU_pub.publish(&data_measured_odom_and_imu);
+    //imu_collection();
+    //data_measured_odom_and_imu.omega_right = average_omega_right;
+    //data_measured_odom_and_imu.omega_left = average_omega_left;
+    //odom_and_IMU_pub.publish(&data_measured_odom_and_imu);
     speed_pub.publish(&wheel_speed);
   }
   nh.spinOnce();
--- arduino_code/slow_dev/data_segmentor_inator/data_segmentor_inator.ino
+++ arduino_code/slow_dev/data_segmentor_inator/data_segmentor_inator.ino
@@ -6,6 +6,7 @@
 int input[46] = {43, 1, 1, 0, 0, 10, 236,  6, 202, 252, 173, 0, 31, 82, 207, 250, 173, 0, 255, 255, 207, 249, 173, 0, 255, 255, 207, 248, 173, 0, 255, 255, 207, 247, 173, 0, 255, 255, 207, 246, 173, 0, 255, 255, 54, 43}; //remove later
 int period = 1000;
 unsigned long time_now = 0;
+enum Byte_Type {Escape = 0x10, StartByte = 0x2, StopByte = 0x3};
 
 ros::NodeHandle nh;
 
@@ -26,21 +27,63 @@ void seperator(int array_input[]){
       beacon_data.RSSI = hex_to_useable_data_test(array_input[i*6+8],0,0);
       beacon_data.distance = 0.343 * hex_to_useable_data_test(array_input[i*6+12],array_input[i*6+13],0);
       beacon_data_pub.publish(&beacon_data);
+      nh.spinOnce();
     }
   }
 }
 
+
+
+void data_colector() {
+  bool esc_char = false;
+  bool start_up = false;
+  int inByte_array[100] = {};
+  int i = 0;
+  while (Serial3.available()) {
+  char inByte = Serial3.read();
+    if (inByte == StartByte || inByte == StopByte || inByte == Escape){
+      if (inByte == Escape) {
+        bool esc_char = true;
+      }
+      if (inByte == StartByte && esc_char == false) {
+        //Serial.print("start|");
+        start_up = true;
+      }
+      if (inByte == StopByte && esc_char == false) {
+        //Serial.println("|stop");
+        start_up = false;
+        int output[i]
+        for (int j = 0;j<i;j++){
+          output[j] = inByte_array[j]
+        }
+        seperator(output);
+      }
+    }
+    else if(((inByte != StartByte && inByte != StopByte) || (esc_char && (inByte == StartByte || inByte==StopByte))) && !start_up)
+    {
+      inByte_array[i] = int(inByte);
+      i = i + 1;
+    //  if ((esc_char == true && inByte != StartByte) || (esc_char == true && inByte != StopByte) ) {
+      esc_char = false;
+    }
+  }
+}
+
+
+
+
+
 void setup() {
+  Serial3.begin(115200);
   nh.initNode();
   nh.advertise(right_tick_pub);
   nh.advertise(beacon_data_pub);
 }
 
 void loop() {
-
-  if(millis() > time_now + period){
+data_colector();
+  /*if(millis() > time_now + period){
     time_now = millis();
-    seperator(input);
-  }
-  nh.spinOnce();
+
+  }*/
 }
