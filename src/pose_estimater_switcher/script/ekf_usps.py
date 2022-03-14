4c9c8e57096661e579ab0f20d72e0e306bdf45a4
romcioo <aleksandra.dudlikk@gmail.com>
2022-03-14 16:30:28 +0100

    mess

--- src/pose_estimater_switcher/script/ekf_usps.py
+++ src/pose_estimater_switcher/script/ekf_usps.py
@@ -3,60 +3,142 @@ import rospy
 from geometry_msgs.msg import Vector3
 from custom_msgs.msg import odom_and_imu
 from custom_msgs.msg import USPS_msgs
+import time
 from datetime import datetime
 import numpy as np
 import math
 from filterpy.kalman import ExtendedKalmanFilter
-
-wheel_speed = [0] * 4
-IMU_data_acc = [0] * 3
-IMU_data_gyro = [0] * 3
-IMU_data_mag = [0] * 3
+from filterpy.common import Q_discrete_white_noise
 
 
-#collecting data neccessary for the kalman filter
-def callback_IMU_acc(data): #from the accelometer
-    global IMU_data_acc
-    IMU_data_acc[0] = data.x
-    IMU_data_acc[1] = data.y
-    IMU_data_acc[2] = data.z
+rotated_matrix = [[0, -1],[1, 0]]
+mag_y_calibrated = 0
+mag_y_calibrated = 0
+pi = 3.141593
+delta_time = 0
+old_time = 0
 
-def callback_IMU_gyro(data): #from the gyroscope
-    global IMU_data_acc
-    IMU_data_gyro[0] = data.x
-    IMU_data_gyro[1] = data.y
-    IMU_data_gyro[2] = data.z
+class USPS_data:
+    def __init__(self):
+        self.id =   [42867, 42928,  42929,  44530,  44531,  44532,  44533,  44534,  44535,  44536,  44537,  44538,  44540]
+        self.x =    [11700, 16244,  7824,   2000,   21369,  26163,  26163,  31000,  35766,  35766,  40205,  40204,  16560]    #[11700  , 16244 , 7824  , 2000  , 21369 , 26163 , 26163 , 31000 , 35766 , 35766 , 40205 , 40204 , 16560 ]
+        self.y =    [5999,  10150,  5726,   4499,   6534,   9939,   3699,   6519,   10012,  3522,   11684,  4363,   3549]    #[5999   , 10150 , 5726  , 4499  , 6534  , 9939  , 3699  , 6519  , 10012  , 10012 , 11684 , 4363  , 3549 ]
+        self.z =    [5577,  5577,   4286,   3530,   5578,   5577,   5577,   5578,   5578,   5578,   3767,   3767,   3767]    #[5577   , 5577  , 4286  , 3530  , 5578  , 5577  , 5577  , 5578  , 5578  , 5578  , 3767  , 3767  , 5577  ]
+        self.count =     [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.distance = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.RSSI =     [0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.pose_est = [16000.0, 6000.0, 300.0]
+        self.pose_est_stored = [[16000.0, 6000.0, 300.0],[16000.0, 6000.0, 300.0],[16000.0, 6000.0, 300.0]]
+        self.pose_predict_from_pose = [0.0, 0.0, 0.0]
+        self.pose_meas_beacon = [0.0, 0.0, 0.0]
+        self.time_i = [3.0, 2.0, 1.0]
+        self.acc_meas = [0.0, 0.0, 0.0]
+        self.omega = [0.0, 0.0]
+        self.r = 0.04
+        self.l = 0.229
+        self.callibration_factor_acc = 1.0
+        self.floor_corection_array = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+        self.floor_corection = [0.0,0.0,0.0]
+        #self.time_unit_convertion_factor = 1
 
-def callback_IMU_mag(data): #from the magnetometer
-    global IMU_data_acc
-    IMU_data_mag[0] = data.x
-    IMU_data_mag[1] = data.y
-    IMU_data_mag[2] = data.z
+    def updating_distance(self, id, rssi, distance):
+        index_of_data = self.id.index(id)
+        #print(index_of_data)
+        self.distance[index_of_data] = distance
+        self.RSSI[index_of_data] = rssi
+        self.count[index_of_data] = 3
+        #print(self.distance[index_of_data])
+        #print("im working")
+
+
+
+class IMU_data:
+    def __init__(self):
+        self.imu_acc = [0]*3
+        self.imu_gyro = [0]*3
+        self.imu_mag = [0]*3
+        self.rotated_matrix = [[0, -1],[1, 0]]
+        self.mag_x_calibrated = 0
+        self.mag_y_calibrated = 0
+        self.pi = 3.141593
+        self.delta_time = 0
+        self.old_time = 0
+
+
+
+    def updating_imu(self, imu_acc, imu_gyro, imu_mag):
+        self.imu_acc = imu_acc
+        self.imu_gyro = imu_gyro
+        self.imu_mag = imu_mag
+
+        #print(self.imu_acc)
+        #print("im working")
+
+# ROS collection
+def callback_imu(data): #from the beacon
+    global imu
+    local_imu_acc = [data.imu_acc.x, data.imu_acc.y, data.imu_acc.z]
+    local_imu_gyro = [data.imu_gyro.x, data.imu_gyro.y, data.imu_gyro.z]
+    local_imu_mag = [data.imu_mag.x, data.imu_mag.y, data.imu_mag.z]
+    imu.updating_imu(data.imu_acc, data.imu_gyro, data.imu_mag)
+
+
 
 def callback_distance(data): #from the beacon
     global w1
     w1.updating_distance(data.ID, data.RSSI, data.distance)
 
 
-#state propagation model
-#x_k = f(x_k_previous,input) + process_noise
 
-#output (measurement) model
-#z_k = h(x_k) + measurement_noise
 
+
+
+
+w1 = USPS_data()
+imu = IMU_data()
 ekf_filter = ExtendedKalmanFilter(5, 3) # number of state vectors - position (x,y), velocity(x), heading(x,y'); measurement variables - mag_x, mag_y, beacon distance
 
 
 
+#velocity = imu_acc[0]
+
+
 def main():
 
-    while not rospy.is_shutdown():
-        rospy.Subscriber("imu_acc", Vector3, callback_IMU_acc)
-        rospy.Subscriber("imu_gyro", Vector3, callback_IMU_gyro)
-        rospy.Subscriber("imu_mag", Vector3, callback_IMU_mag)
-        rospy.Subscriber("beacon_data", USPS_msgs, callback_distance)
-        rate.sleep()
+    global ekf_filter, imu, w1, old_time, delta_time, x, velocity, heading, current_heading, position
 
+    imu.updating_imu([1,1,1],[2,2,2],[3,3,3])
+    #x=1
+    #while True:
+    current_time = time.time()
+    delta_time = time.time() - current_time
+
+#        x+1
+
+    velocity = imu_acc[0] * delta_time
+    print(velocity)
+    current_heading = (math.atan2(imu_mag[0] - mag_y_calibrated, imu_mag[1] - mag_x_calibrated) * 180 / pi) * delta_time
+    heading = rotated_matrix * current_heading * imu_gyro[2] * delta_time
+    position = velocity * heading * delta_time
+    #ekf_filter.x = np.array([[2.], [0.]])       # initial state (location and velocity)
+    #ekf_filter.F = np.array([[1.,1.], [0.,1.]])    # state transition matrix
+    #ekf_filter.H = np.array([[1.,0.]])    # Measurement function
+    #ekf_filter.P *= 1000.                 # covariance matrix
+    #ekf_filter.R = 5                      # state uncertainty
+    #ekf_filter.Q = Q_discrete_white_noise(2, dt, .1) # process uncertainty
+
+    #while not rospy.is_shutdown():
+    #    rospy.Subscriber("imu_data", odom_and_imu, callback_imu)
+    #    rospy.Subscriber("beacon_data", USPS_msgs, callback_distance)
+    #    rate.sleep()
+
+    #while True:
+    #ekf_filter.predict()
+    #ekf_filter.update(get_some_measurement()) #input function
+
+    # do something with the output
+    #x = ekf_filter.x
+    #do_something_amazing(x) #output function
 
 if __name__ == '__main__':
     main()
