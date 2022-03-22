29567845267a1436a3f42c6023717d3cf927f430
9colat <nsaren17@student.aau.dk>
2022-03-22 19:37:32 +0100

    i did a thing thats not fully donebut oh well im off so its fine

--- src/pose_estimater_switcher/script/kf.py
+++ src/pose_estimater_switcher/script/kf.py
@@ -3,17 +3,16 @@ import numpy as np
 # offsets of each variable in the state vector
 iX = 0
 iY = 1
-iZ = 2
-iV = 3
-iHX = 4
-iHY = 5
+iV = 2
+iHX = 3
+iHY = 4
 NUMVARS = iHY + 1
 
 
 class KF:
     def __init__(self, initial_x: float,
                        initial_y: float,
-                       initial_z: float,
+                       #initial_z: float,
                        initial_v: float,
                        initial_h_x: float,
                        initial_h_y: float,
@@ -24,43 +23,61 @@ class KF:
         self._x[iX] = initial_x
         self._x[iV] = initial_v
         self._x[iY] = initial_y
-        self._x[iZ] = initial_z
+        #self._x[iZ] = initial_z
         self._x[iHX] = initial_h_x
         self._x[iHY] = initial_h_y
-
+        self.omega = 0.1
         self._accel_variance = accel_variance
+        self.R90=np.array([[0.0,-1.0],[1.0,0.0]])
+        self.beacon_id = [42867, 42928,  42929,  44530,  44531,  44532,  44533,  44534,  44535,  44536,  44537,  44538,  44540]
+        self.beacon =    np.array([[11700, 16244,  7824,   2000,   21369,  26163,  26163,  31000,  35766,  35766,  40205,  40204,  16560],[5999,  10150,  5726,   4499,   6534,   9939,   3699,   6519,   10012,  3522,   11684,  4363,   3549]])
 
         # covariance of state GRV
-        self._P = np.eye(NUMVARS)
-        # have to update the rest of the mcode from this point un:
+        self._P = np.eye(NUMVARS)#( [1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1])
+
+        # have to update the rest of the code from this point un:
     def predict(self, dt: float) -> None:
         # x = F x
         # P = F P Ft + G Gt a
-        F = np.eye(NUMVARS)
+        #F = np.eye(NUMVARS)
+        print(1.0+self.R90.dot(dt).dot(self.omega))
+        F = np.array([  [1.0, 0.0,  self._x[iHX]*dt,            self._x[iV]*dt,             0.0],
+                        [0.0, 1.0,  self._x[iHY]*dt,            0.0,                          self._x[iV]*dt],
+                        [0.0, 0.0,  1.0,                          0.0,                          0.0],
+                        [0.0, 0.0,  1.0+self.R90.dot(dt).dot(self.omega),   0.0,                          0.0],
+                        [0.0, 0.0,  0.0,                          1.0+self.R90.dot(dt).dot(self.omega),   0.0]])
         #F[iX, iV] = dt
-        F[iX, iY, iZ, iV, iHX, iHY] = dt
+        #F[iX, iY, iZ, iV, iHX, iHY] = dt
         new_x = F.dot(self._x)
 
-        G = np.zeros((2, 1))
-        G[iX] = 0.5 * dt**2
-        G[iV] = dt
+        G = np.array([  [0.5 * dt**2],
+                        [0],
+                        [self._x[iV]],
+                        [0],
+                        [0]])
         new_P = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._accel_variance
 
         self._P = new_P
         self._x = new_x
 
-    def update(self, meas_value: float, meas_variance: float):
+    def update(self, id: int,  meas_value: float, meas_variance: float):
+        index_of_data = self.beacon_id.index(id)
         # y = z - H x
         # S = H P Ht + R
         # K = P Ht S^-1
         # x = x + K y
         # P = (I - K H) * P
 
-        H = np.zeros((1, NUMVARS))
-        H[0, iX] = 1
+        H = np.array([  [np.abs(self.beacon[0,index_of_data]-self._x[iHX])*np.sign(self.beacon[0,index_of_data]-self._x[iHX])*-2],
+                        [np.abs(self.beacon[1,index_of_data]-self._x[iHY])*np.sign(self.beacon[1,index_of_data]-self._x[iHY])*-2],
+                        [0],
+                        [0],
+                        [0]])
 
         z = np.array([meas_value])
         R = np.array([meas_variance])
+        print(self._x)
+        print(len(H),len(H[0]))
 
         y = z - H.dot(self._x)
         S = H.dot(self._P).dot(H.T) + R
--- src/pose_estimater_switcher/script/test_of_kf_class.py
+++ src/pose_estimater_switcher/script/test_of_kf_class.py
@@ -10,7 +10,7 @@ real_x = 0.0
 meas_variance = 0.1 ** 2
 real_v = 0.5
 
-kf = KF(initial_x=0.0, initial_v=1.0, accel_variance=0.1)
+kf = KF(initial_x=0.0, initial_y = 0.0, initial_v=1.0, initial_h_x = 1.0, initial_h_y = 1.0, accel_variance=0.1)
 
 DT = 0.1
 NUM_STEPS = 1000
@@ -20,7 +20,7 @@ mus = []
 covs = []
 real_xs = []
 real_vs = []
-
+i = 0
 for step in range(NUM_STEPS):
     if step > 500:
         real_v *= 0.9
@@ -30,10 +30,16 @@ for step in range(NUM_STEPS):
 
     real_x = real_x + DT * real_v
 
-    kf.predict(dt=DT)
+    kf.predict(dt=float(DT))
     if step != 0 and step % MEAS_EVERY_STEPS == 0:
-        kf.update(meas_value=real_x + np.random.randn() * np.sqrt(meas_variance),
-                  meas_variance=meas_variance)
+        beacon_id = [42867, 42928,  42929,  44530,  44531,  44532,  44533,  44534,  44535,  44536,  44537,  44538,  44540]
+
+        kf.update(  beacon_id[i],
+                    meas_value=real_x + np.random.randn() * np.sqrt(meas_variance),
+                    meas_variance=meas_variance)
+        i = i + 1
+        if i > len(beacon_id):
+            i = 0
 
     real_xs.append(real_x)
     real_vs.append(real_v)
