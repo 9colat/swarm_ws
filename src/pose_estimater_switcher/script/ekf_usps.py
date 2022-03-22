e949f3f9ba3436835b64f0dc07f73da8eb5211ec
romcioo <aleksandra.dudlikk@gmail.com>
2022-03-22 13:31:24 +0100

    testing

--- src/pose_estimater_switcher/script/ekf_usps.py
+++ src/pose_estimater_switcher/script/ekf_usps.py
@@ -7,10 +7,11 @@ import time
 from datetime import datetime
 import numpy as np
 import math
-from autograd import jacobian
+#from autograd import jacobian
+import matplotlib.pyplot as plt
 
-from filterpy.kalman import ExtendedKalmanFilter
-from filterpy.common import Q_discrete_white_noise
+#from filterpy.kalman import ExtendedKalmanFilter
+#from filterpy.common import Q_discrete_white_noise
 
 
 # constants
@@ -20,7 +21,7 @@ mag_y_calibrated = 0.0
 pi = 3.141593
 
 
-class USPS_data:
+class EKF:
     def __init__(self):
 
         #self stuff for beacons
@@ -48,12 +49,13 @@ class USPS_data:
         self.imu_acc = np.array([0.0]*3).reshape(-1,1)
         self.imu_gyro = np.array([0.0]*3).reshape(-1,1)
         self.imu_mag = np.array([0.0]*3).reshape(-1,1)
-        self.old_time = 0.0
+        self.old_time = time.time() / 1000
+        self.predicted_old_time = time.time() / 1000
         self.heading = np.array([1.0]*2).reshape(-1,1)
-        self.position = np.array([0.0]*3).reshape(-1,1)
+        self.position = np.array([0.0]*2).reshape(-1,1)
         self.velocity = 0.0
         self.predicted_heading = np.array([1.0]*2).reshape(-1,1)
-        self.predicted_position = np.array([0.0]*3).reshape(-1,1)
+        self.predicted_position = np.array([0.0]*2).reshape(-1,1)
         self.predicted_velocity = 0.0
         self.input = np.array([0.0]*2)
         self.x_state = np.array([0.0]*3)
@@ -87,10 +89,12 @@ class USPS_data:
         self.heading = self.heading + np.dot(rotated_matrix,self.heading) * self.imu_gyro[2] * delta_time
         self.state_model()
 
+
+
+
     def prediction_model(self):
-        global local_position, local_velocity, local_heading
-        delta_time = time.time() - self.old_time
-        self.old_time = time.time()
+        delta_time = time.time() - self.predicted_old_time
+        self.predicted_old_time = time.time()
 
         self.predicted_position = self.predicted_position + np.dot(self.predicted_velocity,self.predicted_heading) * delta_time
         self.predicted_velocity = self.predicted_velocity + self.imu_acc[0] * delta_time
@@ -109,24 +113,28 @@ class USPS_data:
         self.x_state_predicted = np.transpose([np.transpose(self.predicted_position), self.predicted_velocity, np.transpose(self.predicted_heading)])
 
 
+            self.distance[index_of_data] = distance
+            self.RSSI[index_of_data] = rssi
+            self.count[index_of_data] = 3
+
+
+
     def extended_kalman_filter(self):
-
-    self.measurement = pow(np.linalg.norm(self.position - BEACON), 2)
-    self.measurement_estimated = pow((np.linalg.norm(self.predicted_position - BEACON), 2)
-    estimation_difference = self.measurement - self.measurement_estimated
-
-    B_index=rem(i,3)+1;
-    Beacon=Beacons(:,B_index);
-    #y=norm(states(1:2,i)-Beacon)^2+randn(1)*0.1;
-    #ye=norm(e_states(1:2,i+1)-Beacon)^2;
-    #yd=y-ye;
-    H=HdNum(Beacon(1),Beacon(2),e_states(1,i),e_states(2,i));
+        BEACON = np.array([self.x, self.y]).reshape(-1,1)
+        self.measurement = pow(np.linalg.norm(self.position - BEACON), 2)
+        self.measurement_estimated = pow((np.linalg.norm(self.predicted_position - BEACON), 2)
+        estimation_difference = self.measurement - self.measurement_estimated
 
 
-    S=H*P*H'+R;
-    K=P*H'*inv(S);
-    e_states(:,i+1)=e_states(:,i+1)+K*yd;
-    P=(eye(5)-K*H)*P;
+
+        H=HdNum(Beacon(1),Beacon(2),e_states(1,i),e_states(2,i));
+
+        # define R - covariance matrix
+        # define P - covariance matrix
+        S=H*P*H'+R;
+        K=P*H'*inv(S);
+        e_states(:,i+1)=e_states(:,i+1)+K*yd;
+        P=(eye(5)-K*H)*P;
 
 
 
@@ -134,11 +142,11 @@ class USPS_data:
 
 # ROS collection
 def callback_imu(data): #from the beacon
-    global imu
+    global w1
     local_imu_acc = [data.imu_acc.x, data.imu_acc.y, data.imu_acc.z]
     local_imu_gyro = [data.imu_gyro.x, data.imu_gyro.y, data.imu_gyro.z]
     local_imu_mag = [data.imu_mag.x, data.imu_mag.y, data.imu_mag.z]
-    imu.updating_imu(data.imu_acc, data.imu_gyro, data.imu_mag)
+    w1.updating_imu(data.imu_acc, data.imu_gyro, data.imu_mag)
 
 
 
@@ -151,8 +159,7 @@ def callback_distance(data): #from the beacon
 
 
 
-w1 = USPS_data()
-imu = IMU_data()
+w1 = EKF()
 
 
 
@@ -162,43 +169,30 @@ imu = IMU_data()
 
 def main():
 
+    for x in range(20):
+        w1.updating_imu([1.0,1.0,1.0],[2.0,2.0,2.0],[3.0,3.0,3.0])
+        print(w1.x_state)
+        print(w1.x_state_predicted)
+        time.sleep(2)
+    t = np.arange(0., 5., 0.2)
+    # red dashes, blue squares and green triangles
+    plt.plot(w1.x_state)
+    plt.show()
 
-    ekf_filter = ExtendedKalmanFilter(5, 3) # number of state vectors - position (x,y), velocity(x), heading(x,y'); measurement variables - mag_x, mag_y, beacon distance
-    imu.updating_imu([1.0,1.0,1.0],[2.0,2.0,2.0],[3.0,3.0,3.0])
-    #print(local_position)
-    #print(local_velocity)
-    #print(local_heading)
 
-    #print(rotated_matrix[1])
-    #x=1
-    #while True:
-
-#        x+1
 
     #while not rospy.is_shutdown():
     #    rospy.Subscriber("imu_data", odom_and_imu, callback_imu)
     #    rospy.Subscriber("beacon_data", USPS_msgs, callback_distance)
     #    rate.sleep()
-
-
-    #ekf_filter.x = imu.x_state     # initial state (location and velocity)
-    #ekf_filter.F = np.array([[1.,1.], [0.,1.]])    # state transition matrix
-
-    #ekf_filter.H = np.array([[1.,0.]])    # Measurement function - this is where we should pass beacons and IMU data
-
-    #ekf_filter.P *= 1000.                 # covariance matrix
-    #ekf_filter.R = 5                      # state uncertainty
-    #ekf_filter.Q = Q_discrete_white_noise(2, dt, .1) # process uncertainty
+    # evenly sampled time at 200ms intervals
 
 
 
-    #while True:
-    #ekf_filter.predict()
-    #ekf_filter.update(get_some_measurement()) #input function - using_imu_data()?
 
-    # do something with the output
-    #x = ekf_filter.x
-    #do_something_amazing(x) #output function # ?
-    #print(rotated_matrix)
+
+
+
+
 if __name__ == '__main__':
     main()
