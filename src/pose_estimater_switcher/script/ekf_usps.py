e91cbe91d0cda3175cb2a89d684c9fff272e57f5
romcioo <aleksandra.dudlikk@gmail.com>
2022-03-22 16:00:18 +0100

    it is shit but i will fix it tomorrow

--- src/pose_estimater_switcher/script/ekf_usps.py
+++ src/pose_estimater_switcher/script/ekf_usps.py
@@ -25,7 +25,7 @@ class EKF:
     def __init__(self):
 
         #self stuff for beacons
-        self.id = np.array([42867, 42928,  42929,  44530,  44531,  44532,  44533,  44534,  44535,  44536,  44537,  44538,  44540])
+        self.id = [42867, 42928,  42929,  44530,  44531,  44532,  44533,  44534,  44535,  44536,  44537,  44538,  44540]
         self.x = np.array([11700, 16244,  7824,   2000,   21369,  26163,  26163,  31000,  35766,  35766,  40205,  40204,  16560])    #[11700  , 16244 , 7824  , 2000  , 21369 , 26163 , 26163 , 31000 , 35766 , 35766 , 40205 , 40204 , 16560 ]
         self.y = np.array([5999,  10150,  5726,   4499,   6534,   9939,   3699,   6519,   10012,  3522,   11684,  4363,   3549])    #[5999   , 10150 , 5726  , 4499  , 6534  , 9939  , 3699  , 6519  , 10012  , 10012 , 11684 , 4363  , 3549 ]
         self.z = np.array([5577,  5577,   4286,   3530,   5578,   5577,   5577,   5578,   5578,   5578,   3767,   3767,   3767])    #[5577   , 5577  , 4286  , 3530  , 5578  , 5577  , 5577  , 5578  , 5578  , 5578  , 3767  , 3767  , 5577  ]
@@ -59,8 +59,10 @@ class EKF:
         self.predicted_velocity = 0.0
         self.input = np.array([0.0]*2)
         self.x_state = np.array([0.0]*3)
+        self.x_state_predicted = np.array([0.0]*3)
         self.measurement = 0.0
         self.measurement_estimated = 0.0
+        self.H = 0.0
 
 
 
@@ -69,6 +71,7 @@ class EKF:
         self.distance[index_of_data] = distance
         self.RSSI[index_of_data] = rssi
         self.count[index_of_data] = 3
+        self.extended_kalman_filter(id)
 
     def updating_imu(self, imu_acc, imu_gyro, imu_mag):
         self.imu_acc = imu_acc
@@ -81,8 +84,8 @@ class EKF:
 
 
     def using_imu_data(self):
-        delta_time = time.time() - self.old_time
-        self.old_time = time.time()
+        delta_time = time.time()/1000 - self.old_time
+        self.old_time = time.time()/1000
 
         self.position = self.position + np.dot(self.velocity,self.heading) * delta_time
         self.velocity = self.velocity + self.imu_acc[0] * delta_time
@@ -93,8 +96,8 @@ class EKF:
 
 
     def prediction_model(self):
-        delta_time = time.time() - self.predicted_old_time
-        self.predicted_old_time = time.time()
+        delta_time = time.time()/1000 - self.predicted_old_time
+        self.predicted_old_time = time.time()/1000
 
         self.predicted_position = self.predicted_position + np.dot(self.predicted_velocity,self.predicted_heading) * delta_time
         self.predicted_velocity = self.predicted_velocity + self.imu_acc[0] * delta_time
@@ -108,35 +111,44 @@ class EKF:
 
     def state_model(self):
         self.x_state = np.transpose([np.transpose(self.position), self.velocity, np.transpose(self.heading)])
+        self.extended_kalman_filter()
 
     def predicted_state_model(self):
         self.x_state_predicted = np.transpose([np.transpose(self.predicted_position), self.predicted_velocity, np.transpose(self.predicted_heading)])
+        self.extended_kalman_filter()
 
 
-            self.distance[index_of_data] = distance
-            self.RSSI[index_of_data] = rssi
-            self.count[index_of_data] = 3
+    def extended_kalman_filter(self, id=44531):
 
+        current_beacon = self.id.index(id)
+        delta_time = time.time()/1000 - self.old_time
+        self.old_time = time.time()/1000
+        R = 1.0
+        P = np.identity(5)
+        # jacobians
+        F = [[1,0,delta_time*self.heading[0],delta_time*self.velocity,0],
+             [0,1,delta_time*self.heading[1],0,delta_time*self.velocity],
+             [0,0,1,0,0],
+             [0,0,0,1,-delta_time*self.imu_gyro[2]],
+             [0,0,0,delta_time*self.imu_gyro[2],1]]
+        H_magnetometer = [[0,0,0,self.imu_mag[0],self.imu_mag[1]],
+                          [0,0,0,self.imu_mag[1],-self.imu_mag[0]]]
+        H_distance = [-2 * np.absolute(self.x[current_beacon] - self.position[0]) * np.sign(self.x[current_beacon] - self.position[0]), -2 * np.absolute(self.y[current_beacon] - self.position[1]) * np.sign(self.y[current_beacon] - self.position[1]),0,0,0]
+        BEACON = np.array([self.x[current_beacon], self.y[current_beacon]]).reshape(-1,1)
 
-
-    def extended_kalman_filter(self):
-        BEACON = np.array([self.x, self.y]).reshape(-1,1)
         self.measurement = pow(np.linalg.norm(self.position - BEACON), 2)
-        self.measurement_estimated = pow((np.linalg.norm(self.predicted_position - BEACON), 2)
+        self.measurement_estimated = pow(np.linalg.norm(self.predicted_position - BEACON), 2)
         estimation_difference = self.measurement - self.measurement_estimated
+        self.H = pow(self.x[current_beacon] - self.position[0], 2) + pow(self.y[current_beacon] - self.position[1], 2)
+        S = np.dot(np.dot(self.H, P),np.transpose(self.H)) + R
+        K = np.dot(np.dot(P, np.transpose(self.H)),np.linalg.inv(S))
 
+        self.predicted_position = self.predicted_position + np.dot(K, estimation_difference)
+        self.predicted_velocity = self.predicted_velocity + np.dot(K, estimation_difference)
+        self.predicted_heading = self.predicted_heading + np.dot(K, estimation_difference)
+        P = (np.identity(5) - np.dot(np.dot(K, self.H)), P)
 
-
-        H=HdNum(Beacon(1),Beacon(2),e_states(1,i),e_states(2,i));
-
-        # define R - covariance matrix
-        # define P - covariance matrix
-        S=H*P*H'+R;
-        K=P*H'*inv(S);
-        e_states(:,i+1)=e_states(:,i+1)+K*yd;
-        P=(eye(5)-K*H)*P;
-
-
+        return self.measurement
 
 
 
@@ -163,20 +175,27 @@ w1 = EKF()
 
 
 
-
 #velocity = imu_acc[0]
 
 
 def main():
+    n = 100
+    test_array = [0.0]*n
 
-    for x in range(20):
-        w1.updating_imu([1.0,1.0,1.0],[2.0,2.0,2.0],[3.0,3.0,3.0])
-        print(w1.x_state)
+    for x in range(n):
+        w1.updating_imu([1.0,0.0,0.0],[0.0,0.0,2.0],[1.0,1.0,0.0])
+        w1.updating_distance(44531, 1, 10000)
+        #print(w1.measurement)
+        #print(w1.measurement_estimated)
+        test_array[x] = w1.extended_kalman_filter()
+        plt.scatter(test_array[x],x)
         print(w1.x_state_predicted)
-        time.sleep(2)
-    t = np.arange(0., 5., 0.2)
+        #time.sleep(1)
+    #t = np.arange(0., 5., 0.2)
     # red dashes, blue squares and green triangles
-    plt.plot(w1.x_state)
+
+    plt.xlim([499327316-100, 499327316+100])
+    #print(test_array[n-1])
     plt.show()
 
 
