18e2b0b155ca2d920594f52d49c916a3c090c518
9colat <nsaren17@student.aau.dk>
2022-03-31 13:36:11 +0200

    fixed the rospy.spin error

--- src/pose_estimater_switcher/script/pose_estimator.py
+++ src/pose_estimater_switcher/script/pose_estimator.py
@@ -7,20 +7,14 @@ from sensor_msgs.msg import LaserScan
 from ekf_usps import EKF
 from laser_system import Laser_component
 
-
-
-
 w1 = EKF()
 w2 = Laser_component()
 
 lidar_array = [0.0]*360
 global_time = time.time() # this should be already in seconds, initialised
 
-
-
 # gettng data from the beacons
 def callback_distance(data):
-
     global w1, w2, global_time, lidar_array
     projected_distance = w2.projection(data.ID, data.distance)
     updated_R = w2.potential_occlusion_check(lidar_array, data.ID, [w1.state_predicted[0], w1.state_predicted[1]], [w1.state_predicted[3], w1.state_predicted[4]], data.distance) # measurement 'variance/trust' updated
@@ -31,7 +25,6 @@ def callback_distance(data):
 
     state = w1.beacon_measurement_updater_EKF(data.ID, projected_distance, dT)
     # REMEMBER TO ADD UPDATED_R TO THE FUNCTIONS
-
 
 # getting data from the IMU
 def callback_imu(data):
@@ -52,9 +45,7 @@ def callback_lidar(data): #from the beacon
         lidar_array[i] = data.ranges[i]
 
 
-
 def main():
-
     global w1, w2, global_time
 
     # initialize subscribers
@@ -62,6 +53,7 @@ def main():
     rospy.Subscriber("beacon_data", USPS_msgs, callback_distance)
     rospy.Subscriber("odometry_and_IMU", odom_and_imu, callback_imu)
     rospy.Subscriber("scan", LaserScan, callback_lidar)
+    rate = rospy.Rate(100) # 100hz
 
 
 
@@ -71,9 +63,9 @@ def main():
         local_time = time.time()
         dT = local_time - global_time
         global_time = local_time
-        hello = w1.state_prediction(dT)
-        print(hello)
-        rospy.spin()
+        ola_has_bad_names = w1.state_prediction(dT)
+        #print(ola_has_bad_names)
+        rate.sleep()
 
 
 if __name__ == '__main__':
