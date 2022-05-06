d2c08c2d14b8128d12751e45a41e8e972bd8a061
9colat <nsaren17@student.aau.dk>
2022-05-06 10:57:26 +0200

    push

--- src/pose_estimater_switcher/script/pose_estimator_without_lidar.py
+++ src/pose_estimater_switcher/script/pose_estimator_without_lidar.py
@@ -68,9 +68,10 @@ def main():
         dT = local_time - global_time
         global_time = local_time
         state = w1.state_prediction(dT)
-        print(state[0][0])
-        pose_est.position.x = state[0][0]/1000
-        pose_est.position.y = state[1][0]/1000
+        print("State x: "state[0][0], "y: ",state[1][0])
+        pose_est.position.x = state[0][0]
+        pose_est.position.y = state[1][0]
+        print("ROS x: "pose_est.position.x, "y: ",pose_est.position.y)
         pose_est.position.x = state[2][0]
         pose_est.orientation.x = state[3][0]
         pose_est.orientation.y = state[4][0]
