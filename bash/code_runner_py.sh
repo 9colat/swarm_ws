03c60d1d7ce2f767f89f9ee9f70226863ef5431c
9colat <nsaren17@student.aau.dk>
2022-05-06 11:09:18 +0200

    let me see the rqt_graph

---
+++ bash/code_runner_py.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ip=$1
+
+#nr=1
+echo "logger"
+roslaunch swarm_robot_nav 112.launch 
---
+++ bash/host_ros_pylaunch.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "hey"
+
+in=$1
+
+IP=$(hostname -I)
+read A B C D <<<"${IP//./ }"
+export ROS_IP=$A.$B.$C.$D
+export ROS_MASTER_URI=http://$A.$B.$C.$D:11311
+cd
+./swarm_ws/bash/code_pyrunner.sh
+
+#roslaunch swarm_robot_nav on_robot.launch
+#sleep 60
+#gnome-terminal -x sh -c "./test1.sh; bash"
+#echo "whats wrong with your face?"
+#kill $PPID
