31ee826751bddc3675824a3aac393188dadd423d
9colat <nsaren17@student.aau.dk>
2022-05-08 14:32:44 +0200

    dynamic yeah

---
+++ src/hardware_driver/scripts/pylaunch_dynamic.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import roslaunch
+import rospy
+import time
+import sys
+from pathlib import Path
+from std_msgs.msg import Int16
+
+path = str(Path.home().joinpath("swarm_ws/src/swarm_robot_nav/launch","dynamicTestigAndLog.launch"))
+
+rospy.init_node('en_Mapping', anonymous=True)
+pub1 = rospy.Publisher('indicator_color', Int16, queue_size=10)
+indicator = Int16()
+
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+launch = roslaunch.parent.ROSLaunchParent(uuid, [path])
+launch.start()
+local_time = time.time()
+pub_time = 5
+first_time = True
+desired_time = 20 * 60 # we want the test to be run for 20 min, and convert it to seconds
+test_time_with_over_head_takken_to_a_count = desired_time + (desired_time/100)*25 # we add 25% time to acount for overhead.
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    if (time.time() - local_time) > pub_time and first_time:
+        indicator.data = 1
+        pub1.publish(indicator)
+        first_time = False
+        print("it is now time for a sleep")
+    if (time.time() - local_time) > test_time_with_over_head_takken_to_a_count:
+        print("im awake again")
+        indicator.data = 255
+        pub1.publish(indicator)
+        launch.shutdown()
+        rospy.spin()
+
+
+    rate.sleep()
