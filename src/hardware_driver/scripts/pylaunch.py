54c49af2effcdf2f5e2b22ef866d561a0a0e1702
9colat <nsaren17@student.aau.dk>
2022-05-11 15:28:15 +0200

    why are you gay

--- src/hardware_driver/scripts/pylaunch.py
+++ src/hardware_driver/scripts/pylaunch.py
@@ -5,7 +5,7 @@ import time
 import sys
 from pathlib import Path
 from std_msgs.msg import Int16
-
+print("hello")
 path = str(Path.home().joinpath("swarm_ws/src/swarm_robot_nav/launch","StaticTestigAndLog.launch"))
 
 rospy.init_node('en_Mapping', anonymous=True)
@@ -14,8 +14,11 @@ indicator = Int16()
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
+print("there")
 launch = roslaunch.parent.ROSLaunchParent(uuid, [path])
+print("general")
 launch.start()
+print("kenobi")
 local_time = time.time()
 pub_time = 5
 first_time = True
