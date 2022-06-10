12179b81aeccf25d196f5e42635d6e9319208deb
9colat <nsaren17@student.aau.dk>
2022-06-10 10:28:33 +0200

    hmmm

--- src/hardware_driver/scripts/pylaunch_tuning.py
+++ src/hardware_driver/scripts/pylaunch_tuning.py
@@ -8,6 +8,9 @@ from custom_msgs.msg import Q_R
 
 path = str(Path.home().joinpath("swarm_ws/src/swarm_robot_nav/launch","turning.launch"))
 
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+launch = roslaunch.parent.ROSLaunchParent(uuid, [path])
 rospy.init_node('turning', anonymous=True)
 pub1 = rospy.Publisher('q_and_r', Q_R, queue_size=10)
 pub2 = rospy.Publisher('indicator_color', Int16, queue_size=10)
@@ -17,9 +20,7 @@ indicator = Q_R()
 for r in range(0.0001, 1):
     for q in range(200, 300):
         print("boot")
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
-        launch = roslaunch.parent.ROSLaunchParent(uuid, [path])
+
         launch.start()
         indicator.Q.data = q
         indicator.R.data = r
