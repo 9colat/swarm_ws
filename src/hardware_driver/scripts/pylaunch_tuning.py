ef5a35dc9c3dda395f6d23088e3d44abeb37f304
9colat <nsaren17@student.aau.dk>
2022-06-10 10:03:59 +0200

    fun

--- src/hardware_driver/scripts/pylaunch_tuning.py
+++ src/hardware_driver/scripts/pylaunch_tuning.py
@@ -10,6 +10,8 @@ path = str(Path.home().joinpath("swarm_ws/src/swarm_robot_nav/launch","turning.l
 
 rospy.init_node('turning', anonymous=True)
 pub1 = rospy.Publisher('q_and_r', Q_R, queue_size=10)
+pub2 = rospy.Publisher('indicator_color', Int16, queue_size=10)
+fun = Int16()
 indicator = Q_R()
 
 for r in range(0.0001, 1):
@@ -21,8 +23,12 @@ for r in range(0.0001, 1):
         indicator.Q.data = q
         indicator.R.data = r
         time.sleep(10)
+        fun.data = 1
+        pub2.publish(fun)
         pub1.publish(indicator)
         rospy.spin()
         time.sleep(40)
+        fun.data = 30
+        pub2.publish(fun)
         launch.shutdown()
         time.sleep(10)
